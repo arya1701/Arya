e80e3a034c8019c550a60118caad5054f285e9db
vishal arya <vishal.17bcs1110@abes.ac.in>
2022-09-02 16:01:14 +0530

    THis is my first commit

---
+++ Sample/Welcome.java
@@ -0,0 +1,13 @@
+class Welcome
+{
+  public static void main(String []args){
+
+    System.out.println("Helllo world! byeee");
+    System.out.println("Helllo world! Hii");
+
+    System.out.println("Helllo world! Hii byee");
+
+
+
+  }
+}
